593f294c7c8f521a70e7da8382efba271281d960
Gabriel Rieff <gabrielrieff1@gmail.com>
2024-11-21 21:40:06 -0300

    feat: primeira migration

---
+++ src/EmailServices.Infrastructure/Migrations/DataBaseMigration.cs
@@ -0,0 +1,16 @@
+﻿using EmailServices.Infrastructure.DataAccess;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmailServices.Infrastructure.Migrations;
+
+public class DataBaseMigration 
+{ 
+
+    public static async Task MigrateDataBase(IServiceProvider serviceProvider)
+    {
+        var dbContext = serviceProvider.GetRequiredService<EmailServicesDbContext>();
+
+        await dbContext.Database.MigrateAsync();
+    }
+}
